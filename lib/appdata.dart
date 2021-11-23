0faa6b0a66d8556d57e088e4935996a127e54da1
Matthias <matthivandersanden@gmail.com>
2021-11-23 18:42:32 +0100

    Images

--- lib/appdata.dart
+++ lib/appdata.dart
@@ -27,45 +27,11 @@ class AppData {
     ),
     LevelData(
       song: SongData(
-        title: 'Slave to the Rhythm',
-        artist: 'Michael Jackson',
-        album: 'Xscape',
-        released: '1985',
-        time: '3:57',
-        cover: 'images/sttr.jpg',
-        price: 300,
-      ),
-      difficulty: LevelDifficulty.easy,
-      scores: LevelScores(),
-      colors: LevelColors(
-        text: Colors.white,
-        accent: Color(0xffb6c1c3),
-      ),
-    ),
-    LevelData(
-      song: SongData(
-        title: 'Dive',
-        artist: 'Ed Sheeran',
-        album: 'Divide',
-        released: '2017',
-        time: '3:59',
-        cover: 'images/dive.jpg',
-        price: 400,
-      ),
-      difficulty: LevelDifficulty.medium,
-      scores: LevelScores(),
-      colors: LevelColors(
-        text: Colors.white,
-        accent: Color(0xff83c1e1),
-      ),
-    ),
-    LevelData(
-      song: SongData(
-        title: 'Numb',
-        artist: 'Dotan',
-        album: 'Numb',
-        released: '2020',
-        time: '3:45',
+        title: 'Sparkling Tides',
+        artist: 'Wisp X',
+        album: '97 - Level Up',
+        released: '2015',
+        time: '2:52',
         cover: 'images/numb.jpg',
         price: 150,
       ),
@@ -73,7 +39,41 @@ class AppData {
       scores: LevelScores(),
       colors: LevelColors(
         text: Colors.white,
-        accent: Color(0xff910b11),
+        accent: Color(0xffca5b61),
+      ),
+    ),
+    LevelData(
+      song: SongData(
+        title: 'Epic Trailer',
+        artist: 'Scott Holmes Music',
+        album: 'Cinematic Background Music',
+        released: '2020',
+        time: '1:58',
+        cover: 'images/sttr.jpg',
+        price: 300,
+      ),
+      difficulty: LevelDifficulty.easy,
+      scores: LevelScores(),
+      colors: LevelColors(
+        text: Colors.white,
+        accent: Color(0xff9C93A7),
+      ),
+    ),
+    LevelData(
+      song: SongData(
+        title: 'Throwback',
+        artist: 'Electro-Light',
+        album: 'Throwback',
+        released: '2016',
+        time: '3:35',
+        cover: 'images/dive.jpg',
+        price: 400,
+      ),
+      difficulty: LevelDifficulty.medium,
+      scores: LevelScores(),
+      colors: LevelColors(
+        text: Colors.white,
+        accent: Color(0xff64588a),
       ),
     ),
   ];
