a5db116b8544fc9626ec06deb6c2448f95b167da
aloisvandevoorde <36737105+Aloisvdvoorde@users.noreply.github.com>
2021-11-27 11:04:25 +0100

    i feel like the project is going backwards again 2

--- unity/Music Road/Assets/Scripts/Creator scripts/TestCar.cs
+++ unity/Music Road/Assets/Scripts/Creator scripts/TestCar.cs
@@ -70,24 +70,18 @@ public class TestCar : MonoBehaviour {
             }
         }
         else {
-            if (Input.GetMouseButton(0))
-                reverseTime();
-            else {
-                // calculate absolute distance to next turningpoint
-                int index = turningPositions.Count - 1;
-                if (index != -1)
-                {
-                    Vector3 distance = transform.position - turningPositions[index]; //will throw errors if you revert till the beginning
-                    float totalDistance = distance.x + distance.z;
-                    if (totalDistance < movement * -1)
-                    { // take the movement as guess for next frames movement and don't forget it is always negative here
-                        changeDirection();
-                        transform.position = turningPositions[index];
-                        turningPositions.RemoveAt(index);
-                    }
+            // calculate absolute distance to next turningpoint
+            int index = turningPositions.Count - 1;
+            if (index != -1) {
+                Vector3 distance = transform.position - turningPositions[index]; //will throw errors if you revert till the beginning
+                float totalDistance = distance.x + distance.z;
+                if (totalDistance < movement * -1)
+                { // take the movement as guess for next frames movement and don't forget it is always negative here
+                    changeDirection();
+                    transform.position = turningPositions[index];
+                    turningPositions.RemoveAt(index);
                 }
             }
-            
         }
     }
 
